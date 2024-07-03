518de8ed766a55f562fc8a8d3645f061b7184a3b
SamTF <elseban88@gmail.com>
2024-07-03 19:35:43 +0100

    Improved output message

--- beach-bot.py
+++ beach-bot.py
@@ -50,39 +50,58 @@ async def on_ready():
 ###### COMMANDS        #######################################################
 ### /concerts
 @bot.hybrid_command(name = 'tides', description = 'Check out all the tidal information in your local beach!')
-@app_commands.describe(time_period = 'Check for tides how far out?')
+@app_commands.describe(spot = 'Which beach, dude? 🤙')
+@app_commands.choices(spot=[
+    app_commands.Choice(name='São Pedro de Moel', value='0'),
+    app_commands.Choice(name='Nazaré', value='1'),
+    app_commands.Choice(name='Peniche', value='2'),
+    app_commands.Choice(name='Ericeira', value='3'),
+    app_commands.Choice(name='Cascais', value='4'),
+])
+@app_commands.describe(time_period = 'Check for tides how far out? 🏄')
 @app_commands.choices(time_period=[
     app_commands.Choice(name='today', value='today'),
     app_commands.Choice(name='tomorrow', value='tomorrow'),
     app_commands.Choice(name='weekly', value='weekly'),
 ])
 @app_commands.guilds(discord.Object(id=349267379991347200))
-async def tides(ctx, time_period:app_commands.Choice[str], fruits: Literal['apple', 'banana', 'cherry']):
+async def tides(ctx, spot:app_commands.Choice[str], time_period:app_commands.Choice[str], fruits: Literal['apple', 'banana', 'cherry']) -> None:
     '''
     Displays tidal information for the requested period of time.
     '''
     today = datetime.today().date()
     tomorrow = today + timedelta(days=1)
 
-    print(f'>>> Requesting tides for {time_period.value} by [{ctx.author.name}]')
+    print(f'>>> Requesting tides at {spot.name} for {time_period.value} by [{ctx.author.name}]')
 
     days = []
+    msg = ''
 
     match time_period.value:
         case 'today':
             days = [d for d in DATA if (d.datetime == today)]
+            msg = f'Here are the tides at __{spot.name}__ today, dude 😎\n\n'
+
         case 'tomorrow':
             days = [d for d in DATA if (d.datetime == tomorrow)]
+            msg = f'This is what the waves are gonna look like __tomorrow at {spot.name}__, dude 🤙\n\n'
+
         case 'weekly':
             days = [d for d in DATA if (d.datetime - today).days <= 7]
-        # case 'tomorrow':
-        #     days = [c for c in CONCERTS.values() if c.date.month == today.month]
+            msg = 'Look at all those waves, bro 🌊\n\n'
+
         case _:
-            days = DATA
+            days = [d for d in DATA if (d.datetime == today)]
+            msg = 'Here are the tides today\n\n'
     
-    msg = ''
     for d in days:
-        msg += f"{d}\n\n"
+        formatted_date = d.datetime.strftime("%-d/%-m")
+        msg += f"**__{d.weekday}__**__ ({formatted_date}):__\n"
+
+        for t in d.tides:
+            icon = '🌊' if t.tide == True else '🏖️'
+            bold = '**' if t.tide == False else ''
+            msg += f"{icon}  {bold}{t.time}{bold}  ({t.height})\n"
 
     print(msg)
 
