042891bfeb69c54be8630674aad4c7e1b8ea41e8
SamTF <elseban88@gmail.com>
2024-07-08 16:45:59 +0100

    now accepts parameters instead of hardcoded test data

--- image_generation/pill.py
+++ image_generation/pill.py
@@ -9,7 +9,10 @@ from fonts import Font, FontStyle, FontSize
 from text import TextAnchor, Text
 
 # Date and time
-from datetime import datetime, timedelta
+from datetime import datetime, time
+
+# Typing
+from typing import Dict, Union
 
 ### CONSTANTS for creating the image canvas and formatting other elements
 CANVAS_SIZE = (842, 596)
@@ -41,70 +44,73 @@ TIDE_GRAPH__POS_Y = 442
 HI_TIDE_COLOUR = '#D82E2A'
 LO_TIDE_COLOUR = '#A1CC39'
 
-# test constants
-high_tide = { 'time' : '17:35', 'height' : '3.5' }
-low_tide = { 'time' : '11:08', 'height' : '0.9' }
-
-
-def create_image() -> Image:
+### MAIN FUNCTION
+def create_image(date: str, spot_name: str, temperature: int, high_tide: Dict[str, Union[time, str]], low_tide:  Dict[str, Union[time, str]], today: bool = False) -> Image:
     # CREATING CANVAS
     # canvas = Image.new(MODE, CANVAS_SIZE, BG_COLOUR)
 
-    # Load the starting canvas from disk (for now)
+    # Load the starting canvas from disk
     canvas = Image.open(TEMPLATE).copy()
 
     # Enable editing the image
     draw = ImageDraw.Draw(canvas)
 
-    # Create Text objects
+    # Create Header Text objects
     spot = Text(
-        'São Pedro de Moel',
+        spot_name,
         (30, 127),
         Font(FontStyle.BOLD_CONDENSED, FontSize.LARGE),
         "#FDC017",
         TextAnchor.LEFT
     )
     weekday = Text(
-        'TODAY | July 8',
+        date,
         (30, 59),
         Font(FontStyle.CONDENSED, FontSize.MEDIUM),
         "#FDC017",
         TextAnchor.LEFT
     )
-    temperature = Text(
-        '22º',
+    temp = Text(
+        f'{temperature}º',
         (810, 120),
         Font(FontStyle.BOLD, FontSize.XL),
         "#FDC017",
         TextAnchor.RIGHT
     )
 
-    # Add Text objects to list
-    texts = [spot, weekday, temperature]
-
     # Add text to the canvas
+    texts = [spot, weekday, temp]
     for t in texts:
         draw.text(t.position, t.text, fill=t.colour, font=t.font, anchor=t.anchor)
     
-    # Adding tide graph
+    # Adding the tide graph
     x = tide_graph_x_position(11, 8)
     canvas.paste(TIDE_GRAPH_IMG, (x, 442), mask=TIDE_GRAPH_IMG)
     
-    # Adding day progress marker
-    time_marker = TIME_MARKER_IMG.copy()
-    canvas.paste(time_marker, (get_progress_position(), TIME_MARKER_POS_Y), mask=time_marker)
+    # Adding day progress marker ONLY IF INFORMATION IS FOR TODAY
+    if today:
+        time_marker = TIME_MARKER_IMG.copy()
+        canvas.paste(time_marker, ( get_progress_position(), TIME_MARKER_POS_Y ), mask=time_marker)
 
     # Adding tide markers
     hi_tide_marker = HI_TIDE_MARKER_IMG.copy()
-    canvas.paste(hi_tide_marker, (get_progress_position(datetime.strptime(high_tide['time'], "%H:%M").time()), TIDE_MARKER_POS_Y), mask=hi_tide_marker)
+    canvas.paste(
+        hi_tide_marker,
+        ( get_progress_position(high_tide['time']), TIDE_MARKER_POS_Y ),
+        mask=hi_tide_marker
+    )
 
     lo_tide_marker = LO_TIDE_MARKER_IMG.copy()
-    canvas.paste(lo_tide_marker, (get_progress_position(datetime.strptime(low_tide['time'], "%H:%M").time()), TIDE_MARKER_POS_Y), mask=lo_tide_marker)
+    canvas.paste(
+        lo_tide_marker,
+        ( get_progress_position(low_tide['time']), TIDE_MARKER_POS_Y ),
+        mask=lo_tide_marker
+    )
 
     # Adding tide information
     hi_tide_height = Text(
         high_tide['height'],
-        (get_progress_position(datetime.strptime(high_tide['time'], "%H:%M").time()) + 14, 444),
+        (get_progress_position(high_tide['time']) + 14, 444),
         Font(FontStyle.BOLD_CONDENSED, FontSize.XS),
         HI_TIDE_COLOUR,
         TextAnchor.CENTER   
@@ -112,7 +118,7 @@ def create_image() -> Image:
 
     lo_tide_height = Text(
         low_tide['height'],
-        (get_progress_position(datetime.strptime(low_tide['time'], "%H:%M").time()) + 14, 520),
+        (get_progress_position(low_tide['time']) + 14, 520),
         Font(FontStyle.BOLD_CONDENSED, FontSize.XS),
         LO_TIDE_COLOUR,
         TextAnchor.CENTER
@@ -127,9 +133,10 @@ def create_image() -> Image:
     
     # Add tide times
     for t in [high_tide, low_tide]:
-        text_img = draw_tide_time(t['time'])
+        # text_img = draw_tide_time(t['time'].strftime("%H:%M"))
+        text_img = draw_tide_time(t['time'].strftime("%I:%M"))
 
-        canvas.paste(text_img, (get_progress_position(datetime.strptime(t['time'], "%H:%M").time()) - 2, 448), mask=text_img)
+        canvas.paste(text_img, (get_progress_position(t['time']) - 2, 448), mask=text_img)
 
     
     # DEBUGGING
@@ -166,6 +173,16 @@ def draw_tide_time(time: str) -> Image:
 
 
 def get_progress_position(time: datetime.time = None) -> int:
+    '''
+    This calculates the X-position of the time and tide markers, so that it lines up perfectly with the day's progress.
+
+    Parameters:
+        time (datetime.time): The current time
+    
+    Returns:
+        int: The X-position of the marker image to the nearest integer
+    '''
+
     # Get current time if no time is given
     if not time:
         time = (datetime.now().time())
@@ -216,6 +233,13 @@ def tide_graph_x_position(first_low_tide_hour: int, first_low_tide_minute: int) 
 
 # Debugging
 if __name__ == "__main__":
-    create_image()
+    create_image(
+        'TODAY | July 8',
+        'São Pedro de Moel',
+        22,
+        { 'time' : datetime.strptime('17:35', "%H:%M").time(), 'height' : '3.5' },
+        { 'time' : datetime.strptime('11:08', "%H:%M").time(), 'height' : '0.9' },
+        True
+    )
     # x = tide_graph_x_position(12, 00)
     # print(x)
